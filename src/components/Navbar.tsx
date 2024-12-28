24d853c7cc672d0800a8dbe2a3b52f2385370fb3
John Niyontwali <nijohn0006@gmail.com>
2024-12-29 00:38:03 +0200

    worked on the responsiveness for the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,15 @@ import { ArrowRight, Menu, X, ChevronDown } from 'lucide-react';
 import { logo } from '@/assets';
 import { hideNavbarRoutes, navItems } from '@/constants';
 
+interface NavItem {
+  label: string;
+  path?: string;
+  dropdownItems?: {
+    label: string;
+    path: string;
+  }[];
+}
+
 const Dropdown = ({
   item,
   isMobile = false,
@@ -25,7 +34,7 @@ const Dropdown = ({
 
   return (
     <div
-      className={`relative ${isMobile ? 'w-full' : ''}`}
+      className={`${isMobile ? 'w-full' : 'relative'}`}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
@@ -43,42 +52,70 @@ const Dropdown = ({
         <ChevronDown className={`h-4 w-4 transition-transform duration-500 ${isOpen ? 'rotate-180' : ''}`} />
       </button>
 
-      <div
-        className={`
-          absolute left-0 mt-0
-          transform-gpu transition-all duration-500 ease-in-out
-          ${isMobile ? 'relative w-full mt-2' : 'absolute left-0 mt-0'} 
-          ${isOpen ? 'opacity-100 translate-y-0 visible' : 'opacity-0 -translate-y-2 invisible'}
-          bg-white rounded-lg shadow-lg py-2 z-50 w-48
-        `}
-      >
+      {isMobile ? (
         <div
           className={`
-          absolute bg-white w-3 h-3 rotate-45 top-0 left-1/4 
-          transform -translate-y-1/2
-          transition-opacity duration-500
-          ${isOpen ? 'opacity-100' : 'opacity-0'}
-        `}
-        ></div>
+            overflow-hidden transition-all duration-500
+            ${isOpen ? 'max-h-[500px] opacity-100' : 'max-h-0 opacity-0'}
+          `}
+        >
+          {item.dropdownItems?.map(dropdownItem => (
+            <Link
+              key={dropdownItem.path}
+              to={dropdownItem.path}
+              className={`block pl-4 py-2 text-[1rem] transition-all duration-500 ${
+                location.pathname === dropdownItem.path ? 'text-primary' : 'text-gray-700 hover:text-primary'
+              }`}
+              onClick={() => {
+                setIsOpen(false);
+                onItemClick?.();
+              }}
+            >
+              <span className={location.pathname === dropdownItem.path ? 'text-primary' : ''}>
+                {dropdownItem.label}
+              </span>
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <div
+          className={`
+            absolute left-0 mt-0
+            transform-gpu transition-all duration-500 ease-in-out
+            ${isOpen ? 'opacity-100 translate-y-0 visible' : 'opacity-0 -translate-y-2 invisible'}
+            bg-white rounded-lg shadow-lg py-2 z-50 w-48
+          `}
+        >
+          <div
+            className={`
+              absolute bg-white w-3 h-3 rotate-45 top-0 left-1/4 
+              transform -translate-y-1/2
+              transition-opacity duration-500
+              ${isOpen ? 'opacity-100' : 'opacity-0'}
+            `}
+          ></div>
 
-        {item.dropdownItems?.map(dropdownItem => (
-          <Link
-            key={dropdownItem.path}
-            to={dropdownItem.path}
-            className={`block px-4 py-2 text-[1rem] transition-all duration-500 ${
-              location.pathname === dropdownItem.path
-                ? 'text-primary mx-2'
-                : 'text-gray-700 hover:bg-primary/10 mx-2 rounded-lg'
-            }`}
-            onClick={() => {
-              setIsOpen(false);
-              onItemClick?.();
-            }}
-          >
-            <span className={location.pathname === dropdownItem.path ? 'text-primary' : ''}>{dropdownItem.label}</span>
-          </Link>
-        ))}
-      </div>
+          {item.dropdownItems?.map(dropdownItem => (
+            <Link
+              key={dropdownItem.path}
+              to={dropdownItem.path}
+              className={`block px-4 py-2 text-[1rem] transition-all duration-500 ${
+                location.pathname === dropdownItem.path
+                  ? 'text-primary mx-2'
+                  : 'text-gray-700 hover:bg-primary/10 mx-2 rounded-lg'
+              }`}
+              onClick={() => {
+                setIsOpen(false);
+                onItemClick?.();
+              }}
+            >
+              <span className={location.pathname === dropdownItem.path ? 'text-primary' : ''}>
+                {dropdownItem.label}
+              </span>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
@@ -94,18 +131,13 @@ const Navbar = () => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
 
-      // Initial scroll behavior - hide navbar
       if (currentScrollY > 0 && currentScrollY < 500) {
         setIsHidden(true);
         setIsSticky(false);
-      }
-      // After scrolling past threshold - show sticky navbar
-      else if (currentScrollY >= 500) {
+      } else if (currentScrollY >= 500) {
         setIsHidden(false);
         setIsSticky(true);
-      }
-      // At top - show normal navbar
-      else {
+      } else {
         setIsHidden(false);
         setIsSticky(false);
       }
@@ -161,7 +193,7 @@ const Navbar = () => {
       <div className='max-w-6xl mx-auto flex items-center justify-between px-6 lg:px-0'>
         <div className='flex items-center space-x-10'>
           <Link to='/' className='flex-shrink-0'>
-            <img src={logo} alt='One Health Africa Logo' className='h-[90px] py-2 w-auto' />
+            <img src={logo} alt='Logo' className='h-[90px] py-2 w-auto' />
           </Link>
 
           <div className='hidden lg:flex items-center space-x-8'>{navItems.map(item => renderNavItem(item))}</div>
@@ -192,24 +224,69 @@ const Navbar = () => {
         </div>
       </div>
 
-      {isMobileMenuOpen && (
-        <div className='lg:hidden bg-white shadow-md'>
-          <div className='flex flex-col items-start space-y-4 px-6 py-4'>
-            {navItems.map(item => renderNavItem(item, true))}
+      <div
+        className={`
+          lg:hidden fixed inset-x-0 top-[90px] bg-white shadow-md
+          transition-all duration-500 ease-in-out
+          ${isMobileMenuOpen ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-4 pointer-events-none'}
+          overflow-hidden
+        `}
+        style={{
+          maxHeight: isMobileMenuOpen ? '100vh' : '0',
+          transition: 'max-height 500ms ease-in-out, opacity 500ms ease-in-out, transform 500ms ease-in-out',
+        }}
+      >
+        <div
+          className={`
+          flex flex-col items-start space-y-4 px-6 py-4
+          transform transition-all duration-500 bg-primary/10
+          ${isMobileMenuOpen ? 'translate-y-0' : '-translate-y-4'}
+        `}
+        >
+          {navItems.map((item, index) => (
+            <div
+              key={item.label}
+              className={`
+                w-full transform transition-all duration-500 delay-[${index * 100}ms]
+                ${isMobileMenuOpen ? 'translate-y-0 opacity-100' : '-translate-y-4 opacity-0'}
+              `}
+            >
+              {renderNavItem(item, true)}
+            </div>
+          ))}
 
+          <div
+            className={`
+            w-full pt-2 border-t border-gray-100 transform transition-all duration-500 delay-[${
+              navItems.length * 100
+            }ms]
+            ${isMobileMenuOpen ? 'translate-y-0 opacity-100' : '-translate-y-4 opacity-0'}
+          `}
+          >
             <Link
               to='/projects'
-              className={`text-base font-medium w-full py-2 transition-all duration-500 group ${
-                location.pathname === '/projects' ? 'text-primary' : 'text-gray-700 hover:text-primary'
-              }`}
+              className={`
+                flex items-center justify-between w-full py-3 mb-3
+                text-base font-medium transition-all duration-500
+                ${location.pathname === '/projects' ? 'text-primary' : 'text-gray-700 hover:text-primary'}
+              `}
               onClick={() => setIsMobileMenuOpen(false)}
             >
               <span className={location.pathname === '/projects' ? 'text-primary' : ''}>Projects</span>
-
-              <ArrowRight size={20} className='ml-1 group-hover:translate-x-[3px] transition-all duration-500' />
+              <ArrowRight
+                size={18}
+                className={`
+                  transition-all duration-500
+                  ${isMobileMenuOpen ? 'translate-x-0' : '-translate-x-4'}
+                `}
+              />
             </Link>
 
             <Button
+              className={`
+                w-full transform transition-all duration-500 delay-[${(navItems.length + 1) * 100}ms]
+                ${isMobileMenuOpen ? 'translate-y-0 opacity-100' : '-translate-y-4 opacity-0'}
+              `}
               onClick={() => {
                 setIsMobileMenuOpen(false);
                 navigate('/get-involved');
@@ -219,7 +296,7 @@ const Navbar = () => {
             </Button>
           </div>
         </div>
-      )}
+      </div>
     </nav>
   );
 };
